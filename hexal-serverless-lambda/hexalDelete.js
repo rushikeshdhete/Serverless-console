5e9e640c30fa63a8485fa59f37700c3b10ee48e1
Rushikesh Dhete <rushikesh.dhete@autodesk.com>
2023-01-28 12:34:00 +0530

    first

---
+++ hexal-serverless-lambda/hexalDelete.js
@@ -0,0 +1,39 @@
+const AWS = require('aws-sdk');
+
+
+exports.handler = async(event)=>{
+    const documentClient = new AWS.DynamoDB.documentClient();
+    let responseBody = "";
+    let statusCode = 0;
+
+    const {id} = event.pathParameters;
+
+    const params = {
+        TableName:"Products",
+        Key: {
+            id: id
+        }
+    };
+
+    try{
+        const data = await documentClient.delete(params).promise();
+        responseBody = JSON.stringify(data);
+        statusCode = 204;
+    }catch(err){
+        responseBody = `Unable to delete Products: ${err}`;
+        statusCode = 403;
+    }
+
+    const response = {
+        statusCode : statusCode,
+        headers : {
+            "Content-Type":"application/json"
+        },
+        Body : responseBody
+    };
+
+    return response;
+
+};
+
+
---
+++ hexal-serverless-lambda/hexalGetItems.js
@@ -0,0 +1,33 @@
+const AWS = require('aws-sdk');
+
+exports.handler = async (event)=>{
+
+    const documentClient = new AWS.DynamoDB.DocumentClient();
+
+    let responseBody = "";
+    let statusCode = 0;
+
+
+    const params = {
+        TableName : "Products",
+    };
+
+    try{
+        const data = await documentClient.scan(params).promise();
+        responseBody = JSON.stringify(data);
+        statusCode = 200;
+
+    }catch(err){
+        responseBody = `Unable to get Products: ${err}`;
+        statusCode = 403;
+    }
+
+    const response = {
+        statusCode : statusCode,
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        body : responseBody
+    };
+    return response;
+}
